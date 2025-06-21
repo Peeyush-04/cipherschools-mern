08b0baa71a8d7e6c6f8ae6ba0ea6e56cbe46f1c1
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-06-21 20:20:16 +0530

    Updates

---
+++ src/context/TaskContext.js
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+import { v4 as randomUUID } from "uuid";
+
+const TaskContext = createContext();
+
+const TASK_EDITABLE_FIELD_LIST = ["title", "description"];
+
+const TaskProvider = ({ children }) => {
+  const [taskList, setTaskList] = useState([]);
+
+  const addNewTask = (task) => {
+    setTaskList([
+      ...taskList,
+      { ...task, createdDate: new Date(), taskId: randomUUID },
+    ]);
+  };
+
+  const deleteTask = (taskId) => {
+    setTaskList(taskList.filter((task) => task.taskId !== taskId));
+  };
+
+  const editTask = (task) => {
+    let tempTaskList = [...taskList];
+    // Finding the task
+    for(let t of tempTaskList) {
+        if(t.taskId === task.taskId) {
+            TASK_EDITABLE_FIELD_LIST.forEach((field) => t[field] = task[field]);
+        }
+    }
+    setTaskList(tempTaskList);
+  };
+
+  return (
+    <TaskContext.Provider value={{ taskList, addNewTask, deleteTask, editTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
+
+export { TaskProvider };
+export default TaskContext;
--- src/screens/ToDoScreen.js
+++ src/screens/ToDoScreen.js
@@ -1,37 +1,36 @@
-import { useState } from "react";
+import { useContext } from "react";
 import Task from "../components/Task";
+import TaskContext from "../context/TaskContext";
+import { useNavigate } from "react-router-dom";
 
 const ToDoScreen = () => {
-  const [taskList, setTaskList] = useState([]);
+  const { taskList } = useContext(TaskContext);
+
+  const navigate = useNavigate();
 
   return (
-    <div className="screen">
-      <h1 className="center ui heading">Welcome to ToDo List Page!</h1>
-      <div>
-        <button
-          onClick={(e) => {
-            setTaskList([
-              ...taskList,
-              {
-                title: "Go to gym",
-                description: "Going to gym is good for muscle growth",
-                createdDate: new Date(),
-              },
-            ]);
-          }}
-          className="ui secondary button"
-        >
-          Add Task
-        </button>
-        <section>
-          <div className="ui cards">
-            {taskList.map((task, index) => (
-              <Task task={task} key={index}/>
-            ))}
-          </div>
-        </section>
+    <>
+      <div className="screen">
+        <h1 className="center ui heading">Welcome to ToDo List Page!</h1>
+        <div>
+          <button
+            onClick={(e) => {
+              navigate("/addtask");
+            }}
+            className="ui secondary button"
+          >
+            Add Task
+          </button>
+          <section>
+            <div className="ui cards">
+              {taskList.map((task) => (
+                <Task task={task} key={task.taskId} />
+              ))}
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
