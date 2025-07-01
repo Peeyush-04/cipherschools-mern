4e44dc565c95e074f7d9037b33774701630d0755
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-01 19:13:11 +0530

    clean weather page

--- src/pages/WeatherPage.jsx
+++ src/pages/WeatherPage.jsx
@@ -2,14 +2,8 @@ export default function WeatherPage() {
     return (
         <div className="weather-page">
             <h1 className="heading">Weather Page</h1>
+            {/* Temprature button */}
             
-            <button class="btn btn-soft btn-success">
-                <label className="swap">
-                <input type="checkbox" />
-                <div className="swap-on">Fahrenheit</div>
-                <div className="swap-off">Celcius</div>
-                </label>
-            </button>
         </div>
     );
 }
