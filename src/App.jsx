26a0e649f406bdc3ac627c1c8490c9d7d9253c5a
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-07-03 10:35:18 +0530

    Form logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import WeatherPage from "./pages/WeatherPage";
+import React, {useState} from 'react';
 
 export default function App() {
+
+  const [city, setCity] = useState('');
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    setQuery(city.trim());
+  };
+
   return (
-    <div className="App">
-      <WeatherPage />
+    <div className="app">
+      <h1>Live Weather App</h1>
+      <input 
+        type="text"
+        placeholder="Enter City"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={handleSearch}>Get Weather</button>
+      {query && <WeatherPage city={query} />}
     </div>
   );
 }
